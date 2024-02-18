617e58df35ed55f39bbe2e3ef7a7afe2000c44f2
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-02-18 22:31:12 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//========================== Math Objects  ==========================
+
+// let val;
+
+// val = Math.PI;
+// val = Math.E;
+// val = Math.min(1,2,3,4,5,6,7,8,12);
+// val = Math.max(1,2,3,4,5,6,7,8,12);
+// val = Math.pow(2,3);
+// val = Math.sqrt(81);
+// val = Math.ceil(2.7);     //upper
+// val = Math.floor(2.7);    //lower
+// val = Math.round(3.4);
+// val = Math.round(3.5);
+// val = Math.abs(-10);
+// val = Math.random();
+// val = Math.floor(Math.random() * 100+1);
+
+
+// console.log(val);
+
+
+
+//========================== Date Objects  ==========================
+
+let val;
+let today = new Date();
+val = today;
+
+
+let birthday = new Date('10-10-2000 11:25:27');
+val =birthday;
+
+
+
+
+console.log(val);
+
